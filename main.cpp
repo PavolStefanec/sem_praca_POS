4f55d3bbaae9fd68ca35c8b398d11f272ac432ef
pavol stefanec <palo.stefanec@hotmail.com>
2021-12-18 14:17:39 +0100

    does not work on frios2 server but seems fine

--- main.cpp
+++ main.cpp
@@ -8,124 +8,207 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <pthread.h>
-//#include "server.h"
 
 typedef struct data {
     int jeNaTahu;
+    int pripojenyHrac;
     int posuvaSaPanak;
     int poslednyHod;
+    int pocetHracov;
     bool jeVyherca;
     int tahID;
-    bool zahralTah;
+    int socket;
+    char* buffer;
+    bool* updates;
 
     pthread_mutex_t* mutex;
-    pthread_cond_t* jeHodene;
-    pthread_cond_t* jeOdohrane;
 } DATA;
 
 void* update(void* parData) {
     DATA* data = (DATA*) parData;
-    while (!data->jeVyherca) {
-        pthread_mutex_lock(data->mutex);
-        while (!data->zahralTah) {
-            pthread_cond_wait(data->jeOdohrane, data->mutex);
+
+    int n = -1;
+    int counter = 0;
+    const char *message = (std::to_string(data->jeNaTahu) + std::to_string(data->posuvaSaPanak)
+                           + std::to_string(data->poslednyHod)).c_str();
+    while (n != 0) {
+        counter++;
+        if (counter > 5) {
+            std::cerr << "player is not responding" << std::endl;
+            break;
         }
-        data->jeNaTahu = 1 + data->tahID % 4;
-        data->tahID++;
-        data->poslednyHod = 1 + rand() % 6;
-        //todo poslat hracom info
+        pthead_mutex_lock(data->mutex);
+        n = write(data->socket, message, strlen(message) + 1);
+        if (n != 0) {
+            perror("error writing to socket");
+            sleep(static_cast<unsigned long>(0.25));
+        } else
+            data->updates[data->pripojenyHrac - 1] = true;
         pthread_mutex_unlock(data->mutex);
-        pthread_cond_broadcast(data->jeHodene);
     }
     return nullptr;
 }
 
 void* tah(void *parData) {
     DATA* data = (DATA*) parData;
-    while (!data->jeVyherca) {
-        pthread_mutex_lock(data->mutex);
-        while (true) {
-            pthread_cond_wait(data->jeHodene, data->mutex);
-        }
-        //todo posli update kto, s kym, o kolko....
 
-        pthread_mutex_unlock(data->mutex);
-        pthread_cond_broadcast(data->jeOdohrane);
+    int n = -1;
+    int counter = 0;
+    pthead_mutex_lock(data->mutex);
+    data->poslednyHod = 1 + rand() % 6;
+    const char* message = data->jeNaTahu + "" + ::to_string(data->poslednyHod).c_str();
+    while (n != 0) {
+        counter++;
+        if (counter > 5) {
+            std::cerr << "player is not responding" << std::endl;
+            break;
+        }
+        n = write(data->socket, message, strlen(message) + 1);
+        if (n != 0) {
+            perror("error writing to socket");
+            sleep(static_cast<unsigned long>(0.25));
+        } else {
+            bzero(data->buffer, 256);
+            n = read(data->socket, data->buffer, 255);
+            if (n != 0) {
+                perror("error reading from socket");
+                sleep(static_cast<unsigned long>(0.25));
+            } else {
+                data->posuvaSaPanak = atoi(reinterpret_cast<const char *>(data->buffer[0]));
+                data->jeVyherca = atoi(reinterpret_cast<const char *>(data->buffer[1]));
+
+                data->zahralTah = true;
+                std::cout << "tah bol ukonceny, posunul sa hrac " << data->jeNaTahu << ", s panakom " <<
+                          data->posuvaSaPanak << ", o " << data->poslednyHod << " policka" << std::endl;
+                data->updates[data->jeNaTahu - 1] = true;
+            }
+        }
     }
+    pthread_mutex_unlock(data->mutex);
     return nullptr;
 }
 
 
 int main() {
-//    Server* server = new Server(9999, 1);
-//    server->creatingSocket();
-//    server->binding();
-//    server->connectPlayerToGame();
-//    server->closeSocket();
-//    delete server;
 
+    //// atributes
     int sockfd, newsockfd;
     socklen_t cli_len;
-    //server
     struct sockaddr_in serv_addr;
-    //klienti
     struct sockaddr_in cli_addr;
     int n;
     char buffer[256];
-    bool open;
-    int numberOfPlayers;
-    int lastPlayerId;
+    int pocetHracov = 2;
+    bool updatedAll[pocetHracov];
+    int lastPlayerId = 0;
 
-    open = true;
-    numberOfPlayers = 2;
     bzero((char*)&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(9999);
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        perror("Error creating socket");
-        open = false;
-    }
-
-    if (open) {
-        if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-            perror("Error binding socket address");
-            open = false;
-        }
-    }
-
-    listen(sockfd, 5);
-    cli_len = sizeof(cli_addr);
-    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &cli_len);
-    if (newsockfd < 0) {
-        perror("ERROR on accept");
-        open = false;
-    }
-
-    pthread_t server;
-    pthread_t hraci[numberOfPlayers];
-
+    ////mutex initialization
     pthread_mutex_t mutex;
     pthread_cond_t jeHodene;
     pthread_cond_t jeOdohrane;
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&jeHodene, NULL);
+    pthread_cond_init(&jeOdohrane, NULL);
 
-    pthread_mutex_init(&mutex, nullptr);
-    pthread_cond_init(&jeHodene, nullptr);
-    pthread_cond_init(&jeOdohrane, nullptr);
+    ////threads
+    pthread_t hrac;
 
-    DATA data = {1,0,0, false, 0, true, &mutex, &jeHodene, &jeOdohrane};
+    DATA data = {1, 0, 0, 0, pocetHracov, false, 0, 0, buffer, updatedAll, &mutex, &jeHodene, &jeOdohrane};
 
-    pthread_create(&server, nullptr, &update, &data);
-    for (int i = 0; i < numberOfPlayers; i++) {
-        pthread_create(&hraci[i], nullptr, &tah, &data);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Error creating socket");
+        return 1;
     }
 
-    pthread_join(server, nullptr);
-    for (int i = 0; i < numberOfPlayers; i++) {
-        pthread_join(hraci[i], nullptr);
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Error binding socket address");
+        return 2;
+    }
+
+    while (lastPlayerId != pocetHracov) {
+        listen(sockfd, 5);
+        cli_len = sizeof(cli_addr);
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &cli_len);
+        if (newsockfd < 0) {
+            perror("ERROR on accept");
+            return 3;
+        } else {
+            bzero(buffer, 256);
+            n = read(newsockfd, buffer, 255);
+            if (n < 0) {
+                perror("Error reading from socket");
+                return 7;
+            }
+            if (atoi(buffer) == 0) {
+                lastPlayerId++;
+                const char* message = pocetHracov + "" + lastPlayerId;
+                n = write(newsockfd, message, strlen(message) + 1);
+                if (n != 0) {
+                    perror("error writing to socket");
+                    lastPlayerId--;
+                }
+            } else {
+                const char* message = "000";
+                n = write(newsockfd, message, strlen(message) + 1);
+                if (n != 0)
+                    perror("error writing to socket");
+            }
+        }
+    }
+
+    while (!data.jeVyherca) {
+        listen(sockfd, 5);
+        cli_len = sizeof(cli_addr);
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &cli_len);
+        if (newsockfd < 0) {
+            perror("ERROR on accept");
+            return 3;
+        } else {
+            data.socket = newsockfd;
+
+            bzero(buffer, 256);
+            n = read(newsockfd, buffer, 255);
+            if (n < 0) {
+                perror("Error reading from socket");
+                return 4;
+            }
+            data.pripojenyHrac = atoi(buffer);
+            if (data.pripojenyHrac == data.jeNaTahu && !data.updates[data.pripojenyHrac - 1]) {
+                pthread_create(&hrac, NULL, &tah, &data);
+                pthread_join(hrac, NULL);
+                if (!data.updates[data.pripojenyHrac - 1]) {
+                    std::cerr << "player disconnected from the game... GAME OVER" << std::endl;
+                    return 9999;
+                }
+            } else if (!data.updates[data.pripojenyHrac - 1] && data.updates[data.jeNaTahu - 1]) {
+                pthread_create(&hrac, NULL, &update, &data);
+                pthread_join(hrac, NULL);
+                if (!data.updates[data.pripojenyHrac - 1]) {
+                    std::cerr << "player disconnected from the game... GAME OVER" << std::endl;
+                    return 9999;
+                }
+                bool updatedAll = true;
+                for (int i = 0; i < pocetHracov; i++) {
+                    if (!data.updates[i]) {
+                        updatedAll = false;
+                        break;
+                    }
+                }
+                if (updatedAll) {
+                    data.jeNaTahu = 1 + data.tahID % data.pocetHracov;
+                    data.tahID++;
+                    for (int i = 0; i < pocetHracov; i++) {
+                        data.updates[i] = false;
+                    }
+                }
+            }
+        } //todo zatvorka tu strajkuje
     }
 
     pthread_mutex_destroy(&mutex);
@@ -134,166 +217,3 @@ int main() {
 
     return 0;
 }
-
-
-
-
-//// inet_addr
-//#include <arpa/inet.h>
-//
-//// For threading, link with lpthread
-//#include <pthread.h>
-//#include <semaphore.h>
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
-//#include <sys/socket.h>
-//#include <unistd.h>
-//
-//// Semaphore variables
-//sem_t x, y;
-//pthread_t tid;
-//pthread_t writerthreads[100];
-//pthread_t readerthreads[100];
-//int readercount = 0;
-//
-//// Reader Function
-//void* reader(void* param)
-//{
-//    // Lock the semaphore
-//    sem_wait(&x);
-//    readercount++;
-//
-//    if (readercount == 1)
-//        sem_wait(&y);
-//
-//    // Unlock the semaphore
-//    sem_post(&x);
-//
-//    printf("\n%d reader is inside",
-//           readercount);
-//
-//    sleep(5);
-//
-//    // Lock the semaphore
-//    sem_wait(&x);
-//    readercount--;
-//
-//    if (readercount == 0) {
-//        sem_post(&y);
-//    }
-//
-//    // Lock the semaphore
-//    sem_post(&x);
-//
-//    printf("\n%d Reader is leaving",
-//           readercount + 1);
-//    pthread_exit(NULL);
-//}
-//
-//// Writer Function
-//void* writer(void* param)
-//{
-//    printf("\nWriter is trying to enter");
-//
-//    // Lock the semaphore
-//    sem_wait(&y);
-//
-//    printf("\nWriter has entered");
-//
-//    // Unlock the semaphore
-//    sem_post(&y);
-//
-//    printf("\nWriter is leaving");
-//    pthread_exit(NULL);
-//}
-//
-//// Driver Code
-//int main()
-//{
-//    // Initialize variables
-//    int serverSocket, newSocket;
-//    struct sockaddr_in serverAddr;
-//    struct sockaddr_storage serverStorage;
-//
-//    socklen_t addr_size;
-//    sem_init(&x, 0, 1);
-//    sem_init(&y, 0, 1);
-//
-//    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-//    serverAddr.sin_addr.s_addr = INADDR_ANY;
-//    serverAddr.sin_family = AF_INET;
-//    serverAddr.sin_port = htons(8989);
-//
-//    // Bind the socket to the
-//    // address and port number.
-//    bind(serverSocket,
-//         (struct sockaddr*)&serverAddr,
-//         sizeof(serverAddr));
-//
-//    // Listen on the socket,
-//    // with 40 max connection
-//    // requests queued
-//    if (listen(serverSocket, 50) == 0)
-//        printf("Listening\n");
-//    else
-//        printf("Error\n");
-//
-//    // Array for thread
-//    pthread_t tid[60];
-//
-//    int i = 0;
-//
-//    while (1) {
-//        addr_size = sizeof(serverStorage);
-//
-//        // Extract the first
-//        // connection in the queue
-//        newSocket = accept(serverSocket,
-//                           (struct sockaddr*)&serverStorage,
-//                           &addr_size);
-//        int choice = 0;
-//        recv(newSocket,
-//             &choice, sizeof(choice), 0);
-//
-//        if (choice == 1) {
-//            // Creater readers thread
-//            if (pthread_create(&readerthreads[i++], NULL,
-//                               reader, &newSocket)
-//                != 0)
-//
-//                // Error in creating thread
-//                printf("Failed to create thread\n");
-//        }
-//        else if (choice == 2) {
-//            // Create writers thread
-//            if (pthread_create(&writerthreads[i++], NULL,
-//                               writer, &newSocket)
-//                != 0)
-//
-//                // Error in creating thread
-//                printf("Failed to create thread\n");
-//        }
-//
-//        if (i >= 50) {
-//            // Update i
-//            i = 0;
-//
-//            while (i < 50) {
-//                // Suspend execution of
-//                // the calling thread
-//                // until the target
-//                // thread terminates
-//                pthread_join(writerthreads[i++],
-//                             NULL);
-//                pthread_join(readerthreads[i++],
-//                             NULL);
-//            }
-//
-//            // Update i
-//            i = 0;
-//        }
-//    }
-//
-//    return 0;
-//}
