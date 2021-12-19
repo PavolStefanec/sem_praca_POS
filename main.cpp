3afcf688e43b79567a5ef306541e9e12de6bcee8
Paurikova <miska.paurikova@gmail.com>
2021-12-19 17:35:17 +0100

    I can say, that is is near the end...

--- main.cpp
+++ main.cpp
@@ -16,39 +16,39 @@ typedef struct data {
     int rollDice;
     bool gameIsEnd;
     int moveID;
-    int numberOfUpdates;
     int* newsockfdBuffer;
     char* buffer;
     pthread_mutex_t* mutex;
-    pthread_cond_t* moveEnd;
     pthread_cond_t* gameStart;
-    pthread_cond_t* updateEnd;
 } DATA;
 
 void* hra (void* arg) {
     DATA* data = (DATA*) arg;
     int idPlayer = data->actualPlayer;
-    pthread_mutex_lock(data->mutex);
-    pthread_cond_wait(data->gameStart, data->mutex);
-    pthread_mutex_unlock(data->mutex);
+    printf("ZACINAM %d\n", idPlayer);
+    if (idPlayer != data->numberOfPlayers) {
+        pthread_mutex_lock(data->mutex);
+        pthread_cond_wait(data->gameStart, data->mutex);
+        pthread_mutex_unlock(data->mutex);
+    } else {
+        pthread_mutex_lock(data->mutex);
+        data->actualPlayer = 1;
+        pthread_mutex_unlock(data->mutex);
+        pthread_cond_broadcast(data->gameStart);
+    }
     while(!data->gameIsEnd) {
         if (data->actualPlayer == idPlayer) {
             //tah
-            if (data->moveID != 1) {
-                while(data->numberOfUpdates != (data->numberOfPlayers - 1)) {
-                    pthread_cond_wait(data->updateEnd, data->mutex);
-                }
-            }
-            data->updateEnd = 0;
+            pthread_mutex_lock(data->mutex);
             data->rollDice = 1 + rand() % 6;
             const char* message = ("1/" + std::to_string(data->rollDice)).c_str();
-            int n = write(data->newsockfdBuffer[idPlayer], message, strlen(message) + 1);
+            int n = write(data->newsockfdBuffer[idPlayer - 1], message, strlen(message) + 1);
             if (n < 0) {
                 perror("error writing to socket");
                 return NULL;
             }
             bzero(data->buffer, 256);
-            n = read(data->newsockfdBuffer[idPlayer], data->buffer, 255);
+            n = read(data->newsockfdBuffer[idPlayer -1], data->buffer, 255);
             if (n < 0) {
                 perror("error reading from socket");
                 return NULL;
@@ -57,29 +57,29 @@ void* hra (void* arg) {
             data->gameIsEnd = atoi(&data->buffer[2]);
             std::cout << "Ťah bol ukončený, posunul sa hráč: " << idPlayer<< ", s figúrkou číslo: " <<
                       data->movingFigure << " o: " << data->rollDice << std::endl;
-            //zmena dalsieho hraca
+            //odosli vsetkym info o tvojom tahu
+            for (int i = 0; i < data->numberOfPlayers; i++) {
+                if (i != (data->actualPlayer-1)) {
+                    const char *message = ("2/" + std::to_string(data->actualPlayer) + "/" + std::to_string(data->movingFigure) +
+                                           "/" + std::to_string(data->rollDice)).c_str();
+                    int n = write(data->newsockfdBuffer[i], message, strlen(message) + 1);
+                    printf("Update hraca s id %d sa vykonal\n", i+1);
+                    if (n < 0) {
+                        perror("error writing to socket");
+                        return NULL;
+                    }
+                }
+            }
+            //dalsi hrac
             data->actualPlayer = 1 + data->moveID % data->numberOfPlayers;
             data->moveID++;
-
             pthread_mutex_unlock(data->mutex);
-            pthread_cond_broadcast(data->moveEnd);
-        } else {
-            //update
-            pthread_cond_wait(data->moveEnd, data->mutex);
-            const char* message = ("2/" + std::to_string(data->actualPlayer) + "/" + std::to_string(data->movingFigure) + "/" + std::to_string(data->rollDice)).c_str();
-            int n = write(data->newsockfdBuffer[idPlayer], message, strlen(message) + 1);
-            if (n < 0) {
-                perror("error writing to socket");
-                return NULL;
-            }
-            data->numberOfUpdates ++;
-            pthread_mutex_unlock(data->mutex);
-            pthread_cond_broadcast(data->updateEnd);
         }
     }
-    close(data->newsockfdBuffer[idPlayer]);
+    close(data->newsockfdBuffer[idPlayer - 1]);
     return NULL;
 }
+
 int main(int argc, char* argv[]) {
     srand(time(nullptr));
 
@@ -94,14 +94,14 @@ int main(int argc, char* argv[]) {
     int lastPlayerId = 0;
 
     //nadstavenie poctu hracov
-    if (argc < 1) {
+    if (argc < 2) {
         numberOfPlayers = 2;
     } else {
         numberOfPlayers = atoi(argv[1]);
         if (numberOfPlayers > 4)
             numberOfPlayers = 4;
     }
-    printf("Pocet ocakavanych hracov v spustenej hre LUDO: %d", numberOfPlayers);
+    printf("Pocet ocakavanych hracov v spustenej hre LUDO: %d\n", numberOfPlayers);
 
     int newsockfdBuffer[numberOfPlayers];
 
@@ -123,7 +123,7 @@ int main(int argc, char* argv[]) {
     ////threads
     pthread_t hrac[numberOfPlayers];
 
-    DATA data = {numberOfPlayers, 1, 0, 0, false, 1, 0, newsockfdBuffer, buffer, &mutex, &moveEnd, &gameStart, &updateEnd};
+    DATA data = {numberOfPlayers, 1, 0, 0, false, 1,newsockfdBuffer, buffer, &mutex, &gameStart};
 
     //Socket communication
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -147,14 +147,6 @@ int main(int argc, char* argv[]) {
             workWithSocket = false;
         }
         if (workWithSocket) {
-            bzero(buffer, 256);
-            n = read(newsockfd, buffer, 255);
-            if (n < 0) {
-                perror("Error reading from socket");
-                workWithSocket = false;
-            }
-        }
-        if (workWithSocket) {
             if (numberOfPlayers != lastPlayerId) {
                 lastPlayerId++;
                 const char *message = (std::to_string(numberOfPlayers) + "/" + std::to_string(lastPlayerId)).c_str();
@@ -166,22 +158,18 @@ int main(int argc, char* argv[]) {
                 }
                 if (workWithSocket) {
                     pthread_mutex_lock(data.mutex);
-                    data.newsockfdBuffer[lastPlayerId] = newsockfd;
+                    data.actualPlayer = lastPlayerId;
+                    data.newsockfdBuffer[lastPlayerId - 1] = newsockfd;
                     pthread_mutex_unlock(data.mutex);
 
                     pthread_create(&hrac[lastPlayerId - 1], NULL, &hra, &data);
-                    //mozno nefunguje a treba prestudovat atributy vlakna
-                    pthread_join(hrac[lastPlayerId-1], NULL);
-
-                    if (lastPlayerId == numberOfPlayers) {
-                        pthread_cond_broadcast(data.gameStart);
-                    }
-
                 }
             }
         }
 
     }
+    for (int i = 0; i < numberOfPlayers; i++)
+        pthread_join(hrac[i], NULL);
     close(sockfd);
     pthread_cond_destroy(&gameStart);
     pthread_cond_destroy(&moveEnd);
